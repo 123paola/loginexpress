52375eb9e6e3ea21f6c278e710d70df82843d18c
Pao2023 <andrea_meloo@hotmail.com>
2024-07-15 08:26:22 -0500

    se creo ruta para registrar, listar usuarios y eliminar y se modulo el proyecto

--- app.js
+++ app.js
@@ -1,60 +1,50 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 3000
+
 // Get the client
-const mysql = require('mysql2/promise');
-const cors = require('cors')
-const  sesión  =  require ( 'express-session' )
+
+const cors = require('cors');
+const session = require('express-session')
+const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const login = require('./login');
+const registro = require('./registro');
+const { obtenerUsuarios, eliminarUsuario } = require('./usuarios');
+const validar = require('./validar');
+const saltRounds = 10;
+
+mysql://root:nDdYenrHKErdfohawJSNvnRNvbBZPpSk@monorail.proxy.rlwy.net:42897/railway
+
+
 
 app.use(cors({
-  origin:'http://localhost:5173/',
+  origin:process.env.URLFRONTEND ||'http://localhost:5173',
   credentials:true
 }))
-app.use(session({
-  secret:'jkfkdjhfdfd'
-}))
 
-// Create the connection to database
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'login',
-});
+
+app.use(session({
+    secret:process.env.SECRETSESSION || 'jkfkdjhfdfd',
+    proxy:process.env.NODE_ENV=='production',
+    cookie:{
+      secure:true,
+      sameSite:'none'
+    }
+  }))
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/login',async (req, res) => {
-const datos= req.query;
-// A simple SELECT query
-try {
-  const [results, fields] = await connection.query(
-    "SELECT * FROM `usuarios` WHERE `usuario` =? AND `clave` = ?",
-    [datos.usuario,datos.clave]
-  );
-  if(results.length>0){
-    req.session.usuario = datos.usuario;
-    res.status(200).send('Inicio de sesion correcto')
-  }else{
-    res.status(401).send('Datos incorrectos')
-  }
 
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
-} catch (err) {
-  console.log(err);
-}
-
-  })
-  app.get('/validar', (req, res) => {
-   if(req.session.usuario) {
-    res.status(200).send('sesion validada')
-   }else{
-    res.status(401).send('No autorizado')
-   }
-  
-  })
-
+app.get('/login',login)
+app.get('/validar', validar)
+app.get('/registro', registro)
+app.get('/usuarios', obtenerUsuarios)
+app.delete('/usuarios',eliminarUsuario )
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log('Example app listening on port ${port}')
 })
